5fe996ff86fa6a67ca811069118ddd5337bb2157
Stiv <kirillstavratii@gmail.com>
2024-06-27 10:09:42 +0200

    Add files via upload

--- Project/run.py
+++ Project/run.py
@@ -8,68 +8,68 @@ db = client['demo']
 collection = db['hotels']     
 
 @app.route('/')
-
 def index():
     return render_template('index.html')
 
-def resu(results, guests):
-        resul = []
-        for i in results:
-            x=1
-            for j in range(1,i["Rooms"]):
-                while x==1:
-                    f = f"Room{j}"
-                    if i[f]["available"] == 0 and guests <= i[f]["numberGuests"]:
-                        resul.append(i)
-                        break
-                    
-                
 
-        return resul
-
-
-@app.route('/search', methods=['POST'])
-def search():
-    location = request.form['location']
-    guests = int(request.form['adults'])
-    wifi = request.form['amenities']
-    breakfast = request.form['amenities_2']
-    parking = request.form['amenities']
-    Balkon = request.form['amenities']
-    # rooms = int(request.form['rooms'])
-    comfort = request.form['comfort']
-    print(wifi)
-    print(Balkon)
-    if comfort == "3":
-        comfort = "ThreeStar"
-    # Пример простого запроса к MongoDB
-    query = {
-        "countyName": location,
-        # "max_guests": {"$gte": guests},
-        # "rooms_available": {"$gte": rooms},
-        "HotelRating": comfort
-        # "aviable": 1
-        
-    }
-    print(comfort)
-    results = collection.find(query)
-    results_1 = []
-
+def filtr(results, guests):
+    results_new = []
     for i in results:
         for j in range(1,i["Rooms"]):
             f = f"Room{j}"
             if i[f]["available"] == 0 and guests <= i[f]["numberGuests"]:
-                results_1.append(i)
+                results_new.append(i)
                 print("konic")
                 break
-   
-            
-            
-
-        
+    return results_new
+def convert_stars(comfort):
+    if comfort == "1":
+        return "OneStar"
+    if comfort == "2":
+        return "TwoStar"
+    if comfort == "3":
+        return "ThreeStar"
+    if comfort == "4":
+        return "FourStar"
+    if comfort == "5":
+        return "FiveStar"    
+def filtr_by_amenities(amenities):
+    global wifi
+    wifi = 0
+    global breakfast
+    breakfast = 0
+    global parking
+    parking = 0
+    for i in amenities:
+        match i :
+            case "wifi":
+                wifi = 1
+            case "breakfast":
+                breakfast = 1
+            case "parking":
+                parking = 1
+@app.route('/search', methods=['POST'])
+def search():
+    location = request.form['location']
+    guests = int(request.form['adults'])
+    amenities = request.form.getlist('amenities')
+    # rooms = int(request.form['rooms'])
+    comfort = request.form['comfort']
+    stars = convert_stars(comfort)
+    #filtr_by_amenities(amenities)
+    #print(parking,wifi,breakfast)
+    query = {
+        "countyName": location,
+        "HotelRating": comfort
+        # "Parking":{"$gte": parking},
+        # "FreeWIFI":{"$gte": wifi},
+        # "Breakfast":breakfast
+    }
     
-    
-    return render_template('results.html', results=results_1)
+    results = collection.find(query)
+    results_new = filtr(results,guests)
+    print(results_new)
+    return render_template('results.html', results=results_new)
 
 if __name__ == '__main__':
     app.run(debug=True)
